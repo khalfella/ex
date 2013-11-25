f30e0d536a4b67aae9a91c391ac5b3b01ae5dd6a
Mohamed A. Khalfella <khalfella@gmail.com>
2013-11-26 00:29:14 +0300

    adding exercise comment to ex_5_8 adding ex_5_9

--- chapter_05/ex_5_9.c
+++ chapter_05/ex_5_9.c
@@ -1,10 +1,11 @@
 /*
- * Exercise 5-8. There is no error checking in day_of_year or month_day. 
- * Remedy this defect.
+ * Exercise 5-9. Rewrite the routines day_of_year and month_day
+ * with pointers instead of indexing.
  */
 
+
 #include <stdio.h> 
-static char daytab[2][13] = {
+static char daytab[][13] = {
 	{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
 	{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 };
@@ -17,13 +18,13 @@ int day_of_year(int year, int month, int day)
 
 	/* error checking */
 
-	if ( year < 0 || month > 12 || month < 1  || day < 1 || day > daytab[leap][month]) {
+	if ( year < 0 || month > 12 || month < 1  || day < 1 || day > *((*(daytab+leap))+month)) {
 		/* invalid input */
 		return 0;
 	}
 
 	for (i = 1; i < month; i++)
-		day += daytab[leap][i];
+		day += *((*(daytab+leap))+i);
 	return day;
 }
 
@@ -40,8 +41,8 @@ void month_day(int year, int yearday, int *pmonth, int *pday)
 		return;
 	}
 
-	for (i = 1; yearday > daytab[leap][i]; i++)
-		yearday -= daytab[leap][i];
+	for (i = 1; yearday > *((*(daytab+leap))+i); i++)
+		yearday -= *((*(daytab+leap))+i);
 
 	*pmonth = i;
 	*pday = yearday;
